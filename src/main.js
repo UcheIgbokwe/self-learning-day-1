a6b7033b86c2a7771d83cfc0ea7bb27cdc31e72c
UcheIgbokwe <uchehenryigbokwe@gmail.com>
2017-09-26 17:42:29 +0100

    chore:add test file

--- src/main.js
+++ src/main.js
@@ -1,89 +1,88 @@
 'use strict'
-module.exports = {
 
-    class ZenithAccount{
+class ZenithAccount{
 
-        constructor(accNumber, accName, age){
-            this._accountNumber = accNumber;
-            this._accountName = accName;
-            this._balance = 0;
-            this._age =age;
-
-        }
-
-        get accountName(){
-            return this._accountName;
-        }
-        get accountNumber(){
-            return this._accountNumber;
-        }
-        get balance(){
-            return this._balance;
-        }
-        set age(newAge){
-            this._age = newAge;
-        }
-        get age(){
-            return this._age;
-        }
-        deposit(amount){
-            if(amount > 0){
-                this._balance += amount;
-                return true;
-            }else{
-                return false;
-            }
-        }
-        withdraw(amount){
-            if(amount > this._balance){
-                return false;
-            }else{
-                this._balance -= amount;
-                return true;
-            } 
-        }
+    constructor(accNumber, accName, age){
+        this._accountNumber = accNumber;
+        this._accountName = accName;
+        this._balance = 0;
+        this._age =age;
 
     }
-    class ZecaAccount extends ZenithAccount{
-        constructor (accNumber, accName, accBonus){
-            super (accNumber,accName);
-            this.atmBonus = accBonus;
-        }
-        addBonus(){
-            this.bonus = getbalance() * this.atmBonus/100;
-            deposit(this.bonus);
+
+    get accountName(){
+        return this._accountName;
+    }
+    get accountNumber(){
+        return this._accountNumber;
+    }
+    get balance(){
+        return this._balance;
+    }
+    set age(newAge){
+        this._age = newAge;
+    }
+    get age(){
+        return this._age;
+    }
+    deposit(amount){
+        if(amount > 0){
+            this._balance += amount;
+            return true;
+        }else{
+            return false;
         }
     }
-    class checkingAccount extends ZenithAccount{
+    withdraw(amount){
+        if(amount > this._balance){
+            return false;
+        }else{
+            this._balance -= amount;
+            return true;
+        } 
+    }
 
-        constructor(accNumber,accName){
-            super(accNumber,accName);
+}
+class ZecaAccount extends ZenithAccount{
+    constructor (accNumber, accName, accBonus){
+        super (accNumber,accName);
+        this.atmBonus = accBonus;
+    }
+    addBonus(){
+        this.bonus = getbalance() * this.atmBonus/100;
+        deposit(this.bonus);
+    }
+}
+class checkingAccount extends ZenithAccount{
+
+    constructor(accNumber,accName){
+        super(accNumber,accName);
+        this._transactionCount = 0;
+        this._numFree = 2;
+        this._transFee = 5;
+    }
+    deposit(amount){
+        if (super.deposit(amount)){
+            this._transactionCount++;
+            return true;
+        }
+        return false;
+    }
+    withdraw(amount){
+        if(super.withdraw(amount)){
+            this._transactionCount++;
+            return true;
+        }
+        return false;
+    }
+    deductFees(){
+        if(this_transactionCount > this._numFree){
+            const fees = this._transFee * (this_transactionCount - this._numFree);
+
+        }
+        if(super.withdraw(fees)){
             this._transactionCount = 0;
-            this._numFree = 2;
-            this._transFee = 5;
-        }
-        deposit(amount){
-            if (super.deposit(amount)){
-                this._transactionCount++;
-                return true;
-            }
-            return false;
-        }
-        withdraw(amount){
-            if(super.withdraw(amount)){
-                this._transactionCount++;
-                return true;
-            }
-            return false;
-        }
-        deductFees(){
-            if(this_transactionCount > this._numFree){
-                const fees = this._transFee * (this_transactionCount - this._numFree);
-
-            }
-            if(super.withdraw(fees)){
-                this._transactionCount = 0;
-            }
         }
     }
-}    
+}
+export {ZenithAccount,ZecaAccount,checkingAccount};
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,68 @@
 import {describe, it} from 'mocha';
 import assert from 'assert';
-import * as myApp from '../src/main.js';
+import {ZecaAccount,ZenithAccount,checkingAccount} from '../src/main.js';
+
+describe('ZenithAccount',() => {
+    describe('#ZenithAccount',()=>{
+        it('should be able to return the account name', () => {
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.name, 'Uche');
+        });
+    });
+    describe('#ZenithAccount',() =>{
+        it('should be able to return the account number', () =>{
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.number, 2020871405);
+        });
+    });
+    describe('#ZenithAccount',() =>{
+        it('should be able to return its balance when a deposit or withdrawal is made', () =>{
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.balance, 500);
+        });
+    });
+});
+
+describe('ZecaAccount',() => {
+    describe('#ZecaAccount',()=>{
+        it('should be able to return the account name', () => {
+            let account = new ZecaAccount('Uche', 2020871405, 100);
+            assert.equal(account.name, 'Uche');
+        });
+    });
+    describe('#ZecaAccount',() =>{
+        it('should be able to return the account number', () =>{
+            let account = new ZecaAccount('Uche', 2020871405, 100);
+            assert.equal(account.number, 2020871405);
+        });
+    });
+    describe('#ZecaAccount',() =>{
+        it('should be able to return its balance when a deposit or withdrawal is made', () =>{
+            let account = new ZecaAccount('Uche', 2020871405, 100);
+            assert.equal(account.bonus(30), 650);
+        });
+    });
+});
+
+describe('ZenithAccount',() => {
+    describe('#ZenithAccount',()=>{
+        it('should be able to return Insufficient balance', () => {
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.withdraw(1000), 'Insufficient balance');
+        });
+    });
+    describe('#ZenithAccount',() =>{
+        it('should be able to return balance', () =>{
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.number, 2020871405);
+        });
+    });
+    describe('#ZenithAccount',() =>{
+        it('should be able to return its balance when a deposit or withdrawal is made', () =>{
+            let account = new ZenithAccount('Uche', 2020871405, 500);
+            assert.equal(account.balance, 500);
+        });
+    });
+});
+
+
